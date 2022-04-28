828007d8a74bab6db0d69b97fad21b85cc9aa3b8
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-04-29 03:33:22 +0600

    Create sign-in-up folders

---
+++ src/Components/Register/Providers/RegisterWithEmail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterWithEmail = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegisterWithEmail;
---
+++ src/Components/Register/Providers/RegisterWithGoogle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterWithGoogle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegisterWithGoogle;

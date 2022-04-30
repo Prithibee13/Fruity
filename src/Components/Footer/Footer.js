f05a7621b5bc3d538145ec143bdc9123f9e253ca
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-05-01 02:24:30 +0600

    footer complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,13 +10,22 @@ const Footer = () =>
         <section>
             <footer className='p-5'>
                 <div className="container">
-                    <div className="row">
+                    <div className="row g-5">
                         <div className="col-md-4 col-12">
                             <h3>Copyright &copy; {year}</h3>
                             <h3>Designed and developed by Aziz Prithibee</h3>
                         </div>
-                        <div className="col-md-4 col-12"></div>
-                        <div className="col-md-4 col-12"></div>
+                        <div className="col-md-4 col-12">
+                            <div className="d-flex justify-content-evenly">
+                                <p>Story</p>
+                                <p>Pages</p>
+                                <p>Works</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-12">
+                            <h4>Contact us</h4>
+                            <p>2405 , Jacson Heights , NY</p>
+                        </div>
                     </div>
                 </div>
             </footer>
